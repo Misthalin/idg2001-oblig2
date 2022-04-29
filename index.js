a2015f2917b967a10cf211616ec15f3bd6115546
Kristian Wobbes <kristian.wobbes@gmail.com>
2022-04-29 13:22:53 +0200

    timeout for each measurment

--- index.js
+++ index.js
@@ -1,30 +1,34 @@
-const csv = require('csv-parser');
-const fs = require('fs');
+const csv = require("csv-parser");
+const fs = require("fs");
 
 function getData(file) {
-    let data = [];
-    const iot_data = [];
+  let data = [];
+  const iot_data = [];
 
-    return new Promise((resolve, reject) => {
-        fs.createReadStream(file)
-        .on('error', error => {
-            reject(error);
-        })
-        .pipe(csv())
-        .on('data', (data) => iot_data.push(data))
-        .on('end', () => {
-            resolve(iot_data)
-        });
-    });
+  return new Promise((resolve, reject) => {
+    fs.createReadStream(file)
+      .on("error", (error) => {
+        reject(error);
+      })
+      .pipe(csv())
+      .on("data", (data) => iot_data.push(data))
+      .on("end", () => {
+        resolve(iot_data);
+      });
+  });
 }
 
+const wait = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
+
 async function load_data() {
-    try {
-        const data = await getData('./dataset/household_power_consumption.csv');
-        console.log(data);
-    } catch (error) {
-        console.error(error);
+  const data = await getData("./dataset/household_power_consumption.csv");
+  for (const item in data) {
+    if (Object.hasOwnProperty.call(data, item)) {
+      const element = data[item];
+      console.log(element);
+      await wait(1000);
     }
+  }
 }
 
 load_data();
