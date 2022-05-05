0751a57b1b94d3d87ef34a0268fde72bf4c4a52d
Kristian Wobbes <kristian.wobbes@gmail.com>
2022-05-05 17:53:53 +0200

    heroku!!!

--- server/server.js
+++ server/server.js
@@ -23,7 +23,7 @@ app.get("/temperatures", async (req, res) => {
 });
 
 app.use(express.static(path.join(__dirname, "../frontend/build")));
-app.use("/", (req, res) => {
+app.use("*", (req, res) => {
   res.sendFile(path.join(__dirname, "../frontend/build", "index.html"));
 });
 
