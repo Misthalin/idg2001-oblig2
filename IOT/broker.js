de6072b4808a17e6f4fe71809ef9d57f7c06e97f
Kristian Wobbes <kristian.wobbes@gmail.com>
2022-05-05 19:25:45 +0200

    changed db connection on broker

--- IOT/broker.js
+++ IOT/broker.js
@@ -52,10 +52,14 @@ aedes.on("publish", async (packet) => {
   }
 });
 
-mongoose
-  .connect(process.env.MONGO_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
-  .then(() => console.log("Connected to DB!"))
-  .catch((error) => console.log(error));
+const connectDB = async () => {
+  try {
+    const conn = await mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+
+    console.log(`MongoDB Connected: ${conn.connection.host}`);
+  } catch (error) {
+    console.log(error);
+    process.exit(1);
+  }
+};
+connectDB();
