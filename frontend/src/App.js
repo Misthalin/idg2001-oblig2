af329d9085c2cd2862c607e3ae5cfdd306bb784f
Misthalin <72019688+Misthalin@users.noreply.github.com>
2022-05-05 16:37:42 +0200

    dataset example frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,21 @@
 import './App.css';
 import React from 'react';
 import { LineChart, Line, CartesianGrid, XAxis, YAxis } from 'recharts';
-//import { Chart } from 'react-charts';
-
 
 function App() {
   const data = [
-    { time: '11:23', temp: 24 },
-    { time: '11:24', temp: 25 },
-    { time: '11:25', temp: 22 },
-    { time: '11:26', temp: 17 },
+    { t: '11:23', v: 24 },
+    { t: '11:24', v: 25 },
+    { t: '11:25', v: 22 },
+    { t: '11:26', v: 17 },
   ];
 
 
   return (
     <LineChart width={600} height={300} data={data} margin={{ top: 5, right: 20, bottom: 5, left: 0 }}>
-      <Line type="monotone" dataKey="temp" stroke="#8884d8" />
+      <Line type="monotone" dataKey="v" stroke="#8884d8" />
       <CartesianGrid stroke="#ccc" strokeDasharray="5 5" />
-      <XAxis dataKey="time" />
+      <XAxis dataKey="t" />
       <YAxis />
     </LineChart>
   );
