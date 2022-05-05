df43d6e1fc7215155cf79ae3e1afdd5c78e73193
Kristian Wobbes <kristian.wobbes@gmail.com>
2022-05-05 17:33:37 +0200

    heroku build

--- server/server.js
+++ server/server.js
@@ -3,7 +3,7 @@ require("dotenv").config();
 const connectDB = require("./connectDB");
 const express = require("express");
 const TemperatureModel = require("./temperatureModel");
-
+const path = require("path");
 const app = express();
 const PORT = process.env.PORT || 5050;
 connectDB();
@@ -17,9 +17,14 @@ app.get("/temperatures", async (req, res) => {
     res.status(200);
     res.json({ temperatures });
   } catch (error) {
-    res.status(500)
+    res.status(500);
     res.send("There was a server error when fetching temperatures");
   }
+});
+
+app.use(express.static(path.join(__dirname, "../frontend/build")));
+app.use("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "../frontend/build", "index.html"));
 });
 
 app.listen(PORT, () => console.log(`Server running on port: ${PORT}`));
