71ee3c79e6731ab59a99777a7a0d7433e1798bf3
Kristian Wobbes <kristian.wobbes@gmail.com>
2022-05-06 10:03:13 +0200

    changed data to be displayed

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,7 @@
-import './App.css';
-import React from 'react';
-import axios from 'axios';
-import { LineChart, Line, CartesianGrid, XAxis, YAxis } from 'recharts';
-
+import "./App.css";
+import React from "react";
+import axios from "axios";
+import { LineChart, Line, CartesianGrid, XAxis, YAxis } from "recharts";
 
 class App extends React.Component {
   constructor() {
@@ -12,35 +11,39 @@ class App extends React.Component {
 
   componentDidMount() {
     axios
-      .get('/temperatures')
+      .get("/temperatures")
       .then((res) => {
-        const data = res.data;
+        const result = res.data.temperatures;
+        const data = result.map((item) => {
+          return {
+            t: item.payload.data.t,
+            v: item.payload.data.v,
+          };
+        });
         this.setState({ data });
       })
       .catch((err) => console.log(err));
   }
- /* const data = [
+  /* const data = [
     { t: '11:23', v: 24 },
     { t: '11:24', v: 25 },
     { t: '11:25', v: 22 },
     { t: '11:26', v: 17 }
   ];*/
 
-
   render() {
     return (
       <div>
-      <p>Temperature readings</p>
-      <LineChart width={600} height={300} data={this.state.data} margin={{ top: 5, right: 20, bottom: 5, left: 0 }}>
-        <Line type="monotone" dataKey="v" stroke="#8884d8" />
-        <CartesianGrid stroke="#ccc" strokeDasharray="5 5" />
-        <XAxis dataKey="t" />
-        <YAxis />
-      </LineChart>
+        <p>Temperature readings</p>
+        <LineChart width={600} height={300} data={this.state.data} margin={{ top: 5, right: 20, bottom: 5, left: 0 }}>
+          <Line type="monotone" dataKey="v" stroke="#8884d8" />
+          <CartesianGrid stroke="#ccc" strokeDasharray="5 5" />
+          <XAxis dataKey="t" />
+          <YAxis />
+        </LineChart>
       </div>
     );
   }
-  
 }
 
 export default App;
